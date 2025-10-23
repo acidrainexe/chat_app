5e5b49e8759c96800ab062f03ce89f193ff8315e
naeem <acidallacross@gmail.com>
2025-10-23 01:32:36 -0500

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 💡 FIX: Add your repository name here, including the leading and trailing slash
+  base: '/chat_app/' 
 })
